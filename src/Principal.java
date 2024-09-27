d1f5366ac233374b478b6d05488de3f07b272a9d
Haru <gabs.haru@gmail.com>
2024-09-27 17:01:32 -0400

    Inserção de dados para comprar pelo usuário

--- src/Principal.java
+++ src/Principal.java
@@ -8,6 +8,17 @@ public class Principal {
         double limite = leitura.nextDouble();
         CartaoCredito cartao = new CartaoCredito(limite); // Instanciação de cartão
 
+        int comando = 1;
+        while (comando != 0) {
+            System.out.println("Insira o produto que deseja comprar ");
+            String produto = leitura.nextLine();
+
+            System.out.println("Agora, digite o valor da compra:");
+            double valorCompra = leitura.nextDouble();
+
+            // Instância do produto Compras
+            Compras comprinha = new Compras(produto,valorCompra);
+        }
 
     }
 }
